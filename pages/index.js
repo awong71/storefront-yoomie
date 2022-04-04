bde85ee5ffa9deaf837771e5d321d32f85788a1e
awong71 <albertjameswong@ymail.com>
2022-04-04 14:08:54 -0600

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import {PageTitle} from './../components/PageTitle'
 
+function ProductCard ({name, key, ...props}){
+  return <li>{name}</li>
+}
+
+
+
+
+
 export default function Home() {
+  
+
   return (
     <>
     <PageTitle title="storefront" tagline="featured products"/>
+    {
+      productData.map((uid, name) => <ProductCard key={uid} name={name}/>)
+    }
     </>
   )
 }
