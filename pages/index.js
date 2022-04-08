fac1fa665bae8980dc7d54b2729d3130e2cd42a8
awong71 <albertjameswong@ymail.com>
2022-04-07 22:00:26 -0600

    title

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { ProductCard } from '../components/ProductCard';
 import {PageTitle} from './../components/PageTitle'
 
@@ -13,6 +14,12 @@ export default function Home(props) {
 
   return (
     <>
+    <Head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Storefront</title>
+    </Head>
     <PageTitle tagline="product specials" title="Storefront"/>
     <main>
     { products.map(product=> <ProductCard key={product.uid} product={product}/>)}
@@ -28,6 +35,7 @@ export async function getStaticProps(){
   return {
     props:{
       products
-    }
+    },
+    revalidate: 60
   }
 }
